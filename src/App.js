ab127f959c7575436b8b88dc8e8a30df620ff6c5
Siydul Islam <siydulislam333@gmail.com>
2022-04-10 20:27:43 +0600

    migrated to react18

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import About from './Components/About/About';
+import Blogs from './Components/Blogs/Blogs';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Home from './Components/Home/Home';
+import Reviews from './Components/Reviews/Reviews';
 
 function App() {
   return (
-    <h1>I am app</h1>
+    <div>
+      <h1>I am app</h1>
+      <Home />
+      <Reviews />
+      <Dashboard />
+      <Blogs />
+      <About />
+    </div>
   );
 }
 
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from '../HomePage/HomePage';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>I am Home!</h1>
+            <HomePage />
+        </div>
+    );
+};
+
+export default Home;
